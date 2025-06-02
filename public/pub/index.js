f1e7d5243d0f54c7488e1f82547b866ea421fd18
colonelkarani <quavix0000@gmail.com>
2025-06-03 00:07:46 +0300

    checking ip addr of user

--- public/pub/index.js
+++ public/pub/index.js
@@ -7,7 +7,7 @@ app.use(express.static('pub'))
 
 // Route to handle form submission 
 app.post('/submit-form', (req, res) => { 
-	console.log('Form data received:', req.body); 
+	console.log('Form data received:', req.body  , '\n', `request is from ${req.ip}`); 
 	res.send('Form submitted successfully!'); });
  // Start the server
   app.listen(port, () => {   console.log(`Server running at http://localhost:${port}`); });
